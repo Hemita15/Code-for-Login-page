423c1c039001083915caa8a34853b137f6c4e0cc
Hemita Pathak <hemita.pathak@gmail.com>
2016-07-15 11:16:29 +0530

    LoginScreen with fed in fed out animation

--- LoginScreen/LoginScreen/loginViewController.swift
+++ LoginScreen/LoginScreen/loginViewController.swift
@@ -13,11 +13,15 @@ class loginViewController: UIViewController {
     
     //MARK : Properties
     
+    @IBOutlet weak var login: UILabel!
     
     @IBOutlet weak var txtusername: UITextField!
     
     
     @IBOutlet weak var txtpassword: UITextField!
+    
+    
+    @IBOutlet weak var signin: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +34,49 @@ class loginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewWillAppear(animated: Bool) {
+        
+       /* login.center.x  += view.bounds.width
+        txtusername.center.x  -= view.bounds.width
+        txtpassword.center.x -= view.bounds.width */
+        login.alpha = 0.5
+        txtusername.alpha = 0.5
+        txtpassword.alpha = 0.5
+        //signin.alpha = 0.0
+        signin.transform = CGAffineTransformMakeScale(0.0, 0.0)
+        
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        //title will appear smooth to the screen.
+        
+     //   UIView.animateWithDuration(0.3, animations: {
+       //     self.login.center.x += self.view.bounds.width
+           // self.txtusername.center.x += self.view.bounds.width
+       // })
+        
+       /* UIView.animateWithDuration( 1.5, delay: 0.5, usingSpringWithDamping: 0.3,
+                                   initialSpringVelocity: 0.5,  options: [], animations: {self.txtusername.center.x -= self.view.bounds.width} , completion: nil)
+        
+        UIView.animateWithDuration(1.5, delay: 0.5, options: [], animations: {self.txtpassword.center.x += self.view.bounds.width} , completion: nil) */
+        
+        //alpha for the fade in fade out effect
+        
+        UIView.animateWithDuration(0.5, delay: 0.5,options: [],animations: {self.login.alpha = 1.0}, completion: nil)
+        
+        UIView.animateWithDuration(0.5, delay: 0.5,options: [],animations: {self.txtusername.alpha = 1.0}, completion: nil)
+        
+        UIView.animateWithDuration(0.5, delay: 0.5,options: [],animations: {self.txtpassword.alpha = 1.0}, completion: nil)
+        
+       // UIView.animateWithDuration(0.5, delay: 1.0,options: [],animations: {self.signin.alpha = 1.0}, completion: nil)
+        
+        //transform will show transformed size according to the co-ordinate value.
+        
+        UIView.animateWithDuration(0.5, delay: 0.5,options: [],animations: {self.signin.transform = CGAffineTransformMakeScale(1, 1)}, completion: nil)
+        
+    }
     
     
     //MARK : Actions
